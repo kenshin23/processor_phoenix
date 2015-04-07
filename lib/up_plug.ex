be3ed9efa79483b3dd0362f298accf4669df0e32
Carlos Paparoni <carlos.paparoni@gmail.com>
2015-04-07 17:12:49 -0430

    Test setup callback and added sample files

--- lib/up_plug.ex
+++ lib/up_plug.ex
@@ -1,4 +1,35 @@
 defmodule UpPlug do
   import ExCsv
-  defstruct plug: nil, model: nil, attribute_name: nil
+  defstruct plug: nil, model: nil
+
+  setup do
+    {
+      :ok, \
+      sample_image_plug: %Plug.Upload{ \
+        content_type: "image/png", \
+        filename: "sample.png", \
+        path: "#{Path.expand("../", __DIR__)}/sample_data/sample.png" \
+      }, \
+      sample_csv_plug: %Plug.Upload{ \
+        content_type: "text/csv", \
+        filename: "sample.csv", \
+        path:  "#{Path.expand("../", __DIR__)}/sample_data/sample.csv" \
+      }, \
+      model_struct: %SampleModel{}
+    }
+  end
+
+  setup context do
+    {
+      :ok,
+      Map.put(context, :up_plug, %UpPlug{ \
+        plug: context[:sample_image_plug], \
+        model: context[:model_struct]
+      }) |>
+      Map.put(:csv_up_plug, %UpPlug{ \
+        plug: context[:sample_csv_plug], \
+        model: context[:model_struct]
+      })
+    }
+  end
 end
